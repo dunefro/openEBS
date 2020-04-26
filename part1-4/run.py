05df9b61eeb12a949860d7b56b05009d067487dc
Vedant Pareek <dunefro@gmail.com>
2020-04-26 09:39:48 +0000

    Modified the w+'

--- part1-4/run.py
+++ part1-4/run.py
@@ -6,7 +6,7 @@ def main():
 
     # This will create files named from file1 to file50 in the /apps directory
     for i in range(1,51):
-        file_name = os.path.join('.','file{}'.format(i))
+        file_name = os.path.join('./apps','file{}'.format(i))
         with open(file_name,'w+') as f:
             f.write('The file name is {}'.format(file_name))
 
